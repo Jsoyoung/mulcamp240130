4b41ab70b5b3effa0725bbba2539a734e3164ed8
JeongSoyoung <sspkl124@gmail.com>
2024-01-30 17:09:45 +0900

    UPDATED

--- app.py
+++ app.py
@@ -2,16 +2,49 @@
 
 import streamlit as st
 import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 @st.cache_data
-def load_data():
-    df = sns.load_dataset("iris")
-    return df
+
+def load_data2():
+    prior = pd.read_csv("sample_prior.csv")
+    return prior
+
+def load_data3():
+    products = pd.read_csv("sample_products.csv")
+    return products
 
 def main():
-    st.title("Hello World on Streamlit.io")
-    iris = load_data()
-    st.table(iris)
+    st.title(":cake:Instacart:doughnut:")
+    prior = load_data2()
+    products = load_data3()
+
+    st.header('Order Count by Product')
+    product_id = pd.merge(prior, products, on='product_id', how='inner')
+    grouped_name = product_id.groupby('product_name')['order_id'].nunique().reset_index()
+    st.dataframe(grouped_name.sort_values(by='order_id', ascending=False), use_container_width=True, hide_index=True)
+
+    grouped20 = grouped_name.sort_values(by='order_id', ascending=False).head(20)
+
+
+    show_plot = st.checkbox("상위 20개 차트로 보기")
+    st.write(show_plot)
+
+    fig, ax = plt.subplots(figsize=(35,20))
+    sns.barplot(x='order_id', y='product_name', data=grouped20,ax=ax, palette="viridis")
+    sns.set(font_scale=4)
+    plt.xlabel("Order Count")
+    plt.ylabel("Product")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    if show_plot:
+        st.pyplot(fig)
+    else:
+        st.write("접기")
+    
 
 if __name__ == "__main__":
     main()
